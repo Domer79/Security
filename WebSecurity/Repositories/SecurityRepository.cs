6e4986d4ab1b700a11bdf3f824af3e0a02963bbf
Гарипов Дамир Сагдиевич <garipovd@mail.ru>
2015-03-11 00:24:31 +0500

    еще файлы

---
+++ WebSecurity/Repositories/SecurityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataRepository;
+using WebSecurity.Data;
+
+namespace WebSecurity.Repositories
+{
+    public class SecurityRepository<TEntity> : Repository<TEntity> where TEntity : ModelBase
+    {
+        public SecurityRepository() 
+            : base(new WebMvcSecurityContext())
+        {
+        }
+    }
+}
