2ce90e35d9dc410e29cfdf81d725283f130f9e94
Domer79 <garipovd@mail.ru>
2015-04-17 17:18:48 +0500

    17.04.2015

--- DataRepository/Interfaces/IRepository.cs
+++ DataRepository/Interfaces/IRepository.cs
@@ -2,11 +2,13 @@ using System.Linq;
 
 namespace DataRepository.Interfaces
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> : IQueryable<T> where T : class
     {
         IQueryable<T> AsQueryable();
 
         void InsertOrUpdate(T item);
+
+        void SaveChanges();
 
         void Delete(T item);
 
